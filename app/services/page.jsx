90ca1fea79b62864eca22de4781a5ac04c420c44
f1erfly94 <99speedyz99@gmail.com>
2025-03-19 22:39:14 +0200

    remove link arrows

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -61,10 +61,7 @@ const Services = () => {
                             <div className="w-full flex justify-between items-center">
                                 <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}
                                 </div>
-                                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500
-                                 flex justify-center items-center hover:-rotate-45">
-                                    <BsArrowRight className="text-primary text-3xl" />
-                                </Link>
+
                             </div>
                             <h2 className="text-2xl font-bold">{service.title}</h2>
                             <p className="text-lg">{service.description}</p>
